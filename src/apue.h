16a54e91e8342e44afda4af0b59516ac7d15584c
zgmgypb <zgmgypb@126.com>
2018-04-01 01:14:21 +0800

    upgrade some src files

--- src/apue.h
+++ src/apue.h
@@ -40,7 +40,7 @@
  * */
 #define DIR_MODE (FILE_MODE | S_IXUSR | S_IXGRP | S_IXOTH)
 
-typedef void Sigfunc(inc); /* for signal handlers */
+typedef void Sigfunc(int); /* for signal handlers */
 
 #define min(a, b) ((a) < (b) ? (a) : (b))
 #define max(a, b) ((a) > (b) ? (a) : (b))
@@ -83,5 +83,43 @@ struct termios *tty_termios(void);
 
 int ptym_open(char *, int);
 int ptys_open(char *);
+
+#ifdef TIOCGWINSZ
+pid_t pty_fork(int *, char *, int, const struct termios *, const struct winsize *);
+#endif
+
+int lock_reg(int, int, int, off_t, int, off_t);
+
+#define read_lock(fd, offset, whence, len) lock_reg((fd), F_SETLK, F_RDLCK, (offset), (whence), (len))
+#define readw_lock(fd, offset, whence, len) lock_reg((fd), F_SETLKW, F_RDLCK, (offset), (whence), (len))
+#define write_lock(fd, offset, whence, len) lock_reg((fd), F_SETLK, F_WRLCK, (offset), (whence), (len))
+#define writew_lock(fd, offset, whence, len) lock_reg((fd), F_SETLKW, F_WRLCK, (offset), (whence), (len))
+#define un_lock(fd, offset, whence, len) lock_reg((fd), F_SETLK, F_UNLCK, (offset), (whence), (len))
+
+pid_t lock_test(int, int, off_t, int, off_t);
+
+#define is_read_lockable(fd, offset, whence, len) (lock_test((fd), F_RDLCK, (offset), (whence), (len)) == 0)
+#define is_write_lockable(fd, offset, whence, len) (lock_test((fd), F_WRLCK, (offset), (whence), (len)) == 0)
+
+void err_msg(const char *, ...);
+void err_dump(const char *, ...) __attribute__((noreturn));
+void err_quit(const char *, ...) __attribute__((noreturn));
+void err_cont(int, const char *, ...);
+void err_exit(int, const char *, ...) __attribute__((noreturn));
+void err_ret(const char *, ...);
+void err_sys(const char *, ...) __attribute__((noreturn));
+
+void log_msg(const char *, ...);
+void log_open(const char *, int, int);
+void log_quit(const char *, ...) __attribute__((noreturn));
+void log_ret(const char *, ...);
+void log_sys(const char *, ...) __attribute__((noreturn));
+void log_exit(int, const char *, ...) __attribute__((noreturn));
+
+void TELL_WAIT(void);
+void TELL_PARENT(pid_t);
+void TELL_CHILD(pid_t);
+void WAIT_PARENT(void);
+void WAIT_CHILD(void);
 
 #endif /* _APUE_H */
